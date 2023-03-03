b29cdadd375fa1c3e653b43f9c905169d98ce244
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2023-03-03 15:15:42 +0100

    Added CoordStructure

---
+++ Assets/Scripts/CoordStructure.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scripts/Dimensions")]
+
+public class CoordStructure : MonoBehaviour
+{
+    public int dimensionsNr = 1;
+    public int dimensionSize = 10;
+
+    public List<int[][]> Dimensions()
+    {
+        List<int[][]> dimensions = new();
+
+        for (int i = 0; i < dimensionsNr; i++)
+        {
+            dimensions.Add(new int[dimensionSize-1][]);
+        }
+
+        for (int j = 0; j < dimensionSize - 1; j++)
+        {
+            for (int k = 0; k < dimensionSize - 1; k++)
+            {
+                dimensions[j][k] = new int[dimensionSize - 1];
+            }
+        }
+        return dimensions;
+    }
+}
